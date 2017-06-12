f30e3943f8854ec60f2a9362fac106b5481ecb09
Dom Parfitt <dom.parfitt@cgi.com>
2017-06-12 13:31:32 +0100

    GUI updates

--- src/javafx/NewPlayerPane.java
+++ src/javafx/NewPlayerPane.java
@@ -1,29 +1,35 @@
 package javafx;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class NewPlayerPane extends GridPane {
+	
+	private GameApp app;
+	private TextField nameField;
 
 	public NewPlayerPane(GameApp app) {
-		add(new Label("Name"),0,0);
-		add(new TextField(),1,0);
+		this.app = app;
+		nameField = new TextField();
+		add(new Label("Player Name"),0,0);
+		add(nameField,1,0);
 		
-		Button button = new Button("Join Game");
-		button.setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			public void handle(ActionEvent e) {
-				app.showHandView();
-//				app.play();
-			}
-			
-		});
-		add(button,0,2);
+//		Button button = new Button("Join Game");
+//		button.setOnAction(new EventHandler<ActionEvent>() {
+//
+//			@Override
+//			public void handle(ActionEvent e) {
+//				app.showHandView();
+////				app.play();
+//			}
+//			
+//		});
+//		add(button,0,2);
+	}
+	
+	public String getPlayerName() {
+		return nameField.getText();
 	}
 	
 }
