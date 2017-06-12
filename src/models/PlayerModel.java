2bc769924561af26938552056e5277d32cd01933
Dom Parfitt <dom.parfitt@cgi.com>
2017-06-12 13:31:43 +0100

    Created PlayerModel

---
+++ src/models/PlayerModel.java
@@ -0,0 +1,47 @@
+package models;
+
+import java.util.Observable;
+
+import cards.Card;
+import core.Player;
+
+public class PlayerModel extends Observable {
+	
+	private Player player;
+	private Card playedCard;
+	private boolean hasCard;
+	
+	public PlayerModel(Player player) {
+		this.player = player;
+		this.hasCard = false;
+	}
+	
+	public Player getPlayer() {
+		return this.player;
+	}
+	
+	public synchronized void putCard(Card card) {
+		this.playedCard = card;
+		this.hasCard = true;
+		notifyAll();
+		setChanged();
+		notifyObservers();
+	}
+	
+	public synchronized Card getCard() {
+		setChanged();
+		notifyObservers();
+		while(!hasCard) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		hasCard = false;
+		return playedCard;
+	}
+	
+
+}
