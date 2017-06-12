6dc8d61d73546609b07005bd10a830dd00f45b13
Dom Parfitt <dom.parfitt@cgi.com>
2017-06-12 13:31:19 +0100

    Changes to implement correct screen flow

--- src/javafx/GameApp.java
+++ src/javafx/GameApp.java
@@ -1,18 +1,24 @@
 package javafx;
+import core.Game;
 import core.Player;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import models.GameModel;
 
 public class GameApp extends Application {
 
 	public Stage stage;
+	
+	public MainMenuView mainMenuView;
+	public SinglePlayerSetUpView singlePlayerSetupView;
+	public GameView gameView;
+	
 	public HandView handView;
 	public PlayedCardsView playedCardsView;
 	public NewPlayerPane newPlayerView;
-	public GameModel gameModel;
+	private BoardView boardView;
+	public Game game;
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -23,49 +29,108 @@ public class GameApp extends Application {
 
 		this.stage = stage;
 		
-		stage.setTitle("Hand");
+		stage.setTitle("Sushi Go");
 		
-		Player dom = new Player("Dom");
+		mainMenuView = new MainMenuView(this);
+		singlePlayerSetupView = new SinglePlayerSetUpView(this);
+		
+//		Player dom = new Player("Dom");
 		
 		//Create model
-		gameModel = new GameModel(1);
-		gameModel.addPlayer(dom);
+//		game = new Game(2);
+//	
+//		game.addPlayer(dom);
+//		game.addPlayer(new RandomAIPlayer("CPU"));
 		
 		//Create views
 //		newPlayerView = new NewPlayerPane(this);
-		StartView startView = new StartView(this);
-		handView = new HandView(dom, new HandController(gameModel));
+//		StartView startView = new StartView(this);
+//		handView = new HandView(dom, new HandController(game));
+//		boardView = new BoardView();
 		
-		playedCardsView = new PlayedCardsView(dom);
+//		playedCardsView = new PlayedCardsView(dom);
 		
-		//Add Observers to model
-		gameModel.addObserver(handView);
-		gameModel.addObserver(playedCardsView);
-		gameModel.deal();
+		//Add Observers to model - THIS NEEDS TO BE DONE BEFORE THE GAME IS STARTED
+//		game.addObserver(handView);
+//		dom.addObserver(handView);
+//		game.addObserver(playedCardsView);
+//		game.addObserver(boardView);
+		
+//		game.start();
+//		gameModel.deal();
 
 		//Add Controllers
 //		handView.addController(new HandController(gameModel));
 		
 //		Scene handScene = new Scene(handView);
 		
-		Scene newPlayerScene = new Scene(startView);
-		newPlayerScene.getStylesheets().add(javafx.GameApp.class.getResource("stylesheet.css").toExternalForm());
+//		Scene newPlayerScene = new Scene(boardView);
+//		newPlayerScene.getStylesheets().add(javafx.GameApp.class.getResource("stylesheet.css").toExternalForm());
 //		stage.setScene(newPlayerScene);
-		showHandView();
+//		showHandView();
 		
+		stage.setScene(new Scene(mainMenuView));
 		stage.show();
 		
+	}
+	
+	/**
+	 * Method to change the scene (view)
+	 * @param scene the scene to show
+	 */
+	public void setScene(Scene scene) {
+		stage.setScene(scene);
+	}
+	
+
+	public Game getGame() {
+		return this.game;
+	}
+	
+	public void setGame(Game game) {
+		this.game = game;
+		this.game.addObserver(gameView.getHandView());
+//		game.addObserver(playedCardsView);
+		this.game.addObserver(gameView.getBoardView());
+	}
+	
+	public MainMenuView getMainMenuView() {
+		return mainMenuView;
+	}
+
+	public void setMainMenuView(MainMenuView mainMenuView) {
+		this.mainMenuView = mainMenuView;
+	}
+
+	public SinglePlayerSetUpView getSinglePlayerSetupView() {
+		return singlePlayerSetupView;
+	}
+
+	public void setSinglePlayerSetupView(SinglePlayerSetUpView singlePlayerSetupView) {
+		this.singlePlayerSetupView = singlePlayerSetupView;
+	}
+
+	public GameView getGameView() {
+		return gameView;
+	}
+
+	public void setGameView(GameView gameView) {
+		this.gameView = gameView;
+	}
+	
+	public void setHandView(Player player) {
+		handView = new HandView(player);
 	}
 
 	public void showHandView() {
 		VBox vbox = new VBox();
-		vbox.getChildren().add(playedCardsView);
+		vbox.getChildren().add(boardView);
 		vbox.getChildren().add(handView);
 		stage.setScene(new Scene(vbox));
 	}
 	
 	public void play() {
-		gameModel.play();
+		game.play();
 	}
 	
 }
