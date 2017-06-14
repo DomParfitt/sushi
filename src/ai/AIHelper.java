30016c7de9c3e621207a123a9cdc5d82c4c5b249
Dom Parfitt <dom.parfitt@cgi.com>
2017-06-14 13:15:48 +0100

    Minor tweaks

--- src/ai/AIHelper.java
+++ src/ai/AIHelper.java
@@ -1,19 +1,8 @@
 package ai;
 
-import java.util.List;
-
-import cards.Card;
-
 public class AIHelper {
 	
-//	public static List<Card> depthFirstSearch(List<Card> cards) {
-//		
-//	}
-//	
-//	private static int dfsHelper(List<Card> cards, int highScore) {
-//		if(cards.size() == 1) {
-//			return cards.get(0).getValue();
-//		}
-//	}
-
+	public static void depthFirstTraverse(CardTree tree) {
+		
+	}
 }
