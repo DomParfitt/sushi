1d28288b2752897ea3e33e8a407f0e3e5d2ebd48
Dom Parfitt <dom.parfitt@cgi.com>
2017-06-15 14:37:49 +0100

    Further implementation of the PlayerMessage

--- src/server/PlayerMessage.java
+++ src/server/PlayerMessage.java
@@ -1,30 +1,51 @@
 package server;
 
 import cards.Card;
+import core.Game;
 import core.Player;
 
 /**
- * Object sent by a player to the server indicating a particular
+ * Message sent by a player to the server indicating a particular
  * action they wish to take 
  * @author Dom Parfitt
  *
  */
-public class PlayerAction {
+public class PlayerMessage {
 	
-	public enum PLAYERACTION {PLAYCARD};
+	public enum PlayerAction {CREATE_GAME, VIEW_GAMES, JOIN_GAME, PLAY_CARD, LEAVE_GAME};
 	
+	private PlayerAction action;
+	private int numPlayers;
+	private GameState game;
 	private Player player;
-	private PLAYERACTION action;
 	private Card card;
 	
-	public PlayerAction(Player player, PLAYERACTION action, Card card) {
+	public PlayerMessage(PlayerAction action, Player player, Card card) {
 		this.player = player;
 		this.action = action;
 		this.card = card;
 	}
 	
+	public PlayerMessage(GameState game, Player player) {
+		this.action = PlayerAction.JOIN_GAME;
+		this.game = game;
+		this.player = player;
+	}
+	
+	public PlayerAction getAction() {
+		return this.action;
+	}
+	
+	public int getNumberOfPlayers() {
+		return this.numPlayers;
+	}
+	
 	public Player getPlayer() {
 		return this.player;
+	}
+	
+	public GameState getGame() {
+		return this.game;
 	}
 	
 	public Card getCard() {
