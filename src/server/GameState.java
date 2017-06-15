86cc7457487e6cb7d5a77de885dc19efb872af64
Dom Parfitt <dom.parfitt@cgi.com>
2017-06-15 14:35:15 +0100

    Implemented GameState to act as a 'public' version of a game object

--- src/server/GameState.java
+++ src/server/GameState.java
@@ -1,34 +1,62 @@
 package server;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+
 import core.Game;
 import core.Player;
 
 /**
- * Object sent by the server to each player containing the current state
- * of the game
+ * Object sent by the server to each player containing the current state of the
+ * game
+ * 
  * @author Dom Parfitt
  *
  */
-public class GameState {
-	
+// TODO: Does this need to be observable?
+public class GameState extends Observable implements Serializable {
+
 	private Player recipient;
-	private Game game;
-	
+	private int gameID;
+	private List<Player> players;
+
 	public GameState(Player recipient, Game game) {
 		this.recipient = recipient;
-		this.game = new Game(game.getMaxPlayers());
+		this.gameID = game.getGameID();
+		this.players = new ArrayList<>();
 		for (Player player : game.getPlayers()) {
 			if (player.equals(recipient)) {
-				this.game.addPlayer(player);
+				players.add(player);
 			} else {
-				Player playerCopy = player.getPublicDetails();
-				this.game.addPlayer(playerCopy);
+				players.add(player.getPublicDetails());
 			}
 		}
 	}
-	
-	public Game getGame() {
-		return this.game;
+
+	public Player getRecipient() {
+		return this.recipient;
 	}
 
+	public int getGameID() {
+		return this.gameID;
+	}
+
+	public List<Player> getPlayerDetails() {
+		return this.players;
+	}
+
+	public void update(GameState newState) {
+		if (getGameID() == newState.getGameID()) {
+			for (Player updatedPlayer : newState.getPlayerDetails()) {
+				if (players.contains(updatedPlayer)) {
+					players.remove(updatedPlayer);
+				}
+				players.add(updatedPlayer);
+			}
+			setChanged();
+			notifyObservers();
+		}
+	}
 }
