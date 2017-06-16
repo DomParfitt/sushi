91e437f7a2867b9497639dd199631ed3b3e980c1
Dom Parfitt <dom.parfitt@cgi.com>
2017-06-16 14:04:47 +0100

    Added an empty constructor for displaying an empty space for a card

--- src/javafx/CardButton.java
+++ src/javafx/CardButton.java
@@ -5,6 +5,11 @@ import javafx.scene.control.Button;
 
 public class CardButton extends Button {
 	
+	public CardButton() {
+		setText("CARD BACK");
+		getStyleClass().add("card-button");
+	}
+	
 	public CardButton(Card card) {
 		if (card.getName() == "Maki Roll" || card.getName() == "Nigiri") {
 			setText(card.getName() + " (" + card.getValue() + ")");
