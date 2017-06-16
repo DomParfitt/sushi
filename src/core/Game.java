68dba82c105bf13c975e1be7c6b2330fbb65c4cc
Dom Parfitt <dom.parfitt@cgi.com>
2017-06-16 16:02:56 +0100

    Stopped cards being discarded at end of game (for display purposes)

--- src/core/Game.java
+++ src/core/Game.java
@@ -13,17 +13,18 @@ import cards.Card;
 
 /**
  * Class to represent the core game
+ * 
  * @author Dom Parfitt
  *
  */
 public class Game extends Observable {
-	
+
 	private static int NUMBER_OF_GAMES = 0;
-	
+
 	private int gameID;
 	private CardPool cardPool;
 	private ArrayList<Player> players;
-//	private ArrayList<CardRequester> threads;
+	// private ArrayList<CardRequester> threads;
 	private TreeMap<Player, Card> played;
 	private Deck deck;
 	private Deck discard;
@@ -37,23 +38,22 @@ public class Game extends Observable {
 	public Game(int maxPlayers) {
 		Game.NUMBER_OF_GAMES++;
 		this.gameID = Game.NUMBER_OF_GAMES;
-		
+
 		this.cardPool = new StandardCardPool();
 		this.deck = new Deck(cardPool);
 		this.deck.shuffle();
 		this.discard = new Deck();
 		this.players = new ArrayList<Player>();
-//		this.threads = new ArrayList<>();
+		// this.threads = new ArrayList<>();
 		this.played = new TreeMap<>();
 		this.handSize = 9;
 		this.rounds = 3;
 		this.maxPlayers = maxPlayers;
 	}
-	
+
 	public int getGameID() {
 		return this.gameID;
 	}
-	
 
 	/**
 	 * Adds a player to the game
@@ -66,24 +66,27 @@ public class Game extends Observable {
 		if (players.size() < maxPlayers) {
 			this.players.add(player);
 			successFlag = true;
-//			this.threads.add(new CardRequester(this, player));
+			// this.threads.add(new CardRequester(this, player));
 			notifyAll();
 		}
 		setChanged();
 		notifyObservers();
 		return successFlag;
 	}
-	
+
 	/**
 	 * Adds a card to the played cards pool
-	 * @param player the player who has played the card
-	 * @param card the card played
+	 * 
+	 * @param player
+	 *            the player who has played the card
+	 * @param card
+	 *            the card played
 	 */
 	public synchronized void addPlayedCard(Player player, Card card) {
 		played.put(player, card);
 		notifyAll();
 	}
-	
+
 	@Deprecated
 	public synchronized Map<Player, Card> getPlayedCards() {
 		while (played.size() < maxPlayers) {
@@ -96,26 +99,29 @@ public class Game extends Observable {
 			}
 		}
 		System.out.println("[Game] There are enough played cards now");
-//		Map<Player, Card> output = played;
-//		played.clear();
+		// Map<Player, Card> output = played;
+		// played.clear();
 		return played;
 	}
-	
+
 	/**
 	 * Method for the game to request each player play a card and then wait
 	 * until they have all done so
 	 */
 	public synchronized void requestCards() {
-		//For some reason it doesn't like this
-//		for(CardRequester requester : threads) {
-//			requester.start();
-//		}
+		// For some reason it doesn't like this
+		// for(CardRequester requester : threads) {
+		// requester.start();
+		// }
 		for (Player player : players) {
-			CardRequester requester = new CardRequester(this, player); //TODO: This is inefficient
+			CardRequester requester = new CardRequester(this, player); // TODO:
+																		// This
+																		// is
+																		// inefficient
 			requester.start();
 		}
 
-		//Block until all requests have returned
+		// Block until all requests have returned
 		while (played.size() < maxPlayers) {
 			try {
 				wait();
@@ -124,100 +130,116 @@ public class Game extends Observable {
 			}
 		}
 	}
-	
+
 	/**
 	 * Gets a list of the current players in the game
+	 * 
 	 * @return
 	 */
-	//TODO: Does this expose too much?
+	// TODO: Does this expose too much?
 	public List<Player> getPlayers() {
 		return this.players;
 	}
-	
+
 	/**
 	 * Gets a particular player
-	 * @param player the player object to retrieve
+	 * 
+	 * @param player
+	 *            the player object to retrieve
 	 * @return the player object
 	 */
-	//TODO: Does this expose too much?
+	// TODO: Does this expose too much?
 	@Deprecated
 	public Player getPlayer(Player player) {
 		int index = players.indexOf(player);
 		return players.get(index);
 	}
-	
+
 	/**
 	 * Gets the hand of a particular player
-	 * @param player the player to get the hand of
+	 * 
+	 * @param player
+	 *            the player to get the hand of
 	 * @return an ArrayList of Cards representing the player's hand
 	 */
-	//TODO: Check if this is necessary, could easily allow AI to 'cheat'
+	// TODO: Check if this is necessary, could easily allow AI to 'cheat'
 	public ArrayList<Card> getHand(Player player) {
 		int index = players.indexOf(player);
-		if(index >= 0) {
+		if (index >= 0) {
 			return players.get(index).getHand();
 		} else {
 			return new ArrayList<Card>();
 		}
 	}
-	
+
 	/**
 	 * Gets the played cards of a particular player
-	 * @param player the player to get the played cards of
+	 * 
+	 * @param player
+	 *            the player to get the played cards of
 	 * @return an ArrayList of Cards representing the player's played cards
 	 */
 	public ArrayList<Card> getPlayedCards(Player player) {
 		int index = players.indexOf(player);
-		if(index >= 0) {
+		if (index >= 0) {
 			return players.get(index).getPlayedCards();
 		} else {
 			return new ArrayList<Card>();
 		}
 	}
-	
+
 	/**
 	 * Get the maximum number of players for the game
+	 * 
 	 * @return the max players
 	 */
 	public int getMaxPlayers() {
 		return this.maxPlayers;
 	}
-	
+
 	/**
 	 * Sets the maximum number of players for the game
-	 * @param maxPlayers the maximum number of players
+	 * 
+	 * @param maxPlayers
+	 *            the maximum number of players
 	 */
 	public void setMaxPlayers(int maxPlayers) {
 		this.maxPlayers = maxPlayers;
 	}
-	
+
 	/**
 	 * Gets the starting hand size for the game
+	 * 
 	 * @return the hand size for the game
 	 */
 	public int getHandSize() {
 		return this.handSize;
 	}
-	
+
 	/**
 	 * Sets the starting hand size for the game
-	 * @param handSize the new starting hand size
+	 * 
+	 * @param handSize
+	 *            the new starting hand size
 	 */
 	public void setHandSize(int handSize) {
 		this.handSize = handSize;
 	}
-	
+
 	/**
 	 * Gets the number of rounds
+	 * 
 	 * @return the number of rounds
 	 */
 	public int getNumberOfRounds() {
 		return this.rounds;
 	}
-	
+
 	/**
 	 * Sets the number of rounds
-	 * @param rounds the number of rounds
+	 * 
+	 * @param rounds
+	 *            the number of rounds
 	 */
 	public void setNumberOfRounds(int rounds) {
 		this.rounds = rounds;
@@ -230,11 +252,12 @@ public class Game extends Observable {
 	 * @return a Card object, the top card on the deck
 	 */
 	private Card dealCard() {
-		//TODO: Deck and discard pile are empty
-		
+		// TODO: Deck and discard pile are empty
+
 		// If the deck is empty switch with the discard pile and clear it
 		if (this.deck.getDeckSize() <= 0) {
-			this.deck = this.discard; //TODO: This doesn't work (copies reference rather than making a copy)
+			this.deck = this.discard; // TODO: This doesn't work (copies
+										// reference rather than making a copy)
 			this.discard = new Deck();
 		}
 
@@ -258,7 +281,8 @@ public class Game extends Observable {
 	 * Rotates each player's hand around
 	 */
 	private void switchHands() {
-		//TODO: Could this be done with a request so that getHand could be potentially removed?
+		// TODO: Could this be done with a request so that getHand could be
+		// potentially removed?
 		ArrayList<ArrayList<Card>> hands = new ArrayList<ArrayList<Card>>();
 		for (Player player : this.players) {
 			hands.add(player.getHand());
@@ -269,7 +293,7 @@ public class Game extends Observable {
 		}
 		setChanged();
 		notifyObservers();
-		
+
 	}
 
 	/**
@@ -285,9 +309,10 @@ public class Game extends Observable {
 	}
 
 	/**
-	 * Adds all the played cards of the Players provided to the
-	 * discard pile
-	 * @param players a list of players
+	 * Adds all the played cards of the Players provided to the discard pile
+	 * 
+	 * @param players
+	 *            a list of players
 	 */
 	private void discardPlayedCards(List<Player> players) {
 		for (Player player : players) {
@@ -299,39 +324,32 @@ public class Game extends Observable {
 	/**
 	 * Play the game
 	 * 
-	 * Pre-requisites: 
-	 * 1) At least 2 players 
-	 * 2) Enough cards (Deck size >= num Players * hand size)
+	 * Pre-requisites: 1) At least 2 players 2) Enough cards (Deck size >= num
+	 * Players * hand size)
 	 * 
-	 * Order of play: 
-	 * 1) Deal each player a hand 
-	 * 2) Each player plays a card(series/parallel depending on implementation) 
-	 * 3) Players pass hands 
-	 * 4) Continue until no cards left 
-	 * 5) Calculate basic score for each player 
-	 * 6) Assign Maki bonuses 
-	 * 7) Add played hands to discard pile 
-	 * (Opt) Show running scores 
-	 * 8) Repeat 1-7 as many times as there are rounds 
-	 * 9) Assign pudding bonus/forfeit 
-	 * 10) Show final scores, declare winner
+	 * Order of play: 1) Deal each player a hand 2) Each player plays a
+	 * card(series/parallel depending on implementation) 3) Players pass hands
+	 * 4) Continue until no cards left 5) Calculate basic score for each player
+	 * 6) Assign Maki bonuses 7) Add played hands to discard pile (Opt) Show
+	 * running scores 8) Repeat 1-7 as many times as there are rounds 9) Assign
+	 * pudding bonus/forfeit 10) Show final scores, declare winner
 	 */
 	public void play() {
 
-		//Scanner to get user input
+		// Scanner to get user input
 		Scanner in = new Scanner(System.in);
-		
+
 		for (int round = 0; round < this.rounds; round++) {
-			
+
 			System.out.println("////////////////////////////");
 			System.out.println("//        ROUND " + (round + 1) + "         //");
 			System.out.println("////////////////////////////");
-			
+
 			// 1) Deal each player a hand
 			deal();
 
 			// 2) Each player plays a card
-			
+
 			for (int i = 0; i < this.handSize; i++) {
 				for (Player player : this.players) {
 					System.out.println(player);
@@ -358,7 +376,7 @@ public class Game extends Observable {
 
 			// 7) Add played hands to discard pile
 			discardPlayedCards(players);
-			
+
 		} // 8) Repeat 1-7 for each round
 
 		// 9) Assign pudding bonus/forfeit
@@ -367,77 +385,87 @@ public class Game extends Observable {
 		// 10) Show final scores
 		Score.showScores(players);
 
-		//Close scanner
+		// Close scanner
 		in.close();
 	}
-	
+
 	/**
-	 * Public method to start the game. Creates a new thread
-	 * which handles the actual starting and running of the game
+	 * Public method to start the game. Creates a new thread which handles the
+	 * actual starting and running of the game
 	 */
 	public synchronized void start() {
 		GameThread thread = new GameThread(this);
 		thread.start();
 	}
-	
+
 	/**
 	 * Starts the game
 	 */
 	public synchronized void startGame() {
 		System.out.println("Game will start when enough players join");
-		
-		//Blocks until enough players have joined
-		while(players.size() != maxPlayers) {
+
+		// Blocks until enough players have joined
+		while (players.size() != maxPlayers) {
 			try {
-				System.out.println(players.size() + " players have joined, waiting for " + (maxPlayers - players.size() + " more"));
+				System.out.println(players.size() + " players have joined, waiting for "
+						+ (maxPlayers - players.size() + " more"));
 				wait();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
 		System.out.println("Starting game");
-		
-		//Plays each round
-		for(int round = 0; round < rounds; round++) {
+
+		// Plays each round
+		for (int round = 1; round <= rounds; round++) {
 			deal();
-			
-			//Repeats until all held cards have been played
-			for(int i = 0; i < handSize; i++) {
-				
-				//Request cards from each player and block until they are all received
+
+			// Repeats until all held cards have been played
+			for (int i = 0; i < handSize; i++) {
+
+				// Request cards from each player and block until they are all
+				// received
 				requestCards();
-				
-				//Update each player with their played card
-				for(Player player : played.keySet()) {
+
+				// Update each player with their played card
+				for (Player player : played.keySet()) {
 					player.playCard(played.get(player));
 				}
-				
-				//All cards are played so reset the Map
+
+				// All cards are played so reset the Map
 				played.clear();
-				
+
 				switchHands();
 			}
-			
+
 			Score.getRoundScore(players);
 
-			Score.showScores(players);
-			
+//			Score.showScores(players);
+
 			try {
-				wait(5000);
+				wait(3000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			discardPlayedCards(players);
+			if (round != rounds) {
+				discardPlayedCards(players);
+			}
 		}
-		
+
 		Score.addPuddings(players);
 
-		Score.showScores(players);
+//		Score.showScores(players);
 		setChanged();
 		notifyObservers();
 
-		
 	}
 	
+	public void newGame() {
+		discardPlayedCards(players);
+		this.deck = new Deck(cardPool);
+		this.deck.shuffle();
+		start();
+	}
+
 }
